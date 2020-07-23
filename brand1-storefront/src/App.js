20d78872ebbd8628d789b2ebf67e9458ecda7d31
Emily <emilyk@nutranext.net>
2020-07-22 20:46:19 -0400

    install components

--- brand1-storefront/src/App.js
+++ brand1-storefront/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@brand1/main/lib/Button";
+import Input from "@brand1/main/lib/Input";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button>Test</Button>
-      </header>
+      <Input secondary />
+      <Button primary>Test</Button>
     </div>
   );
 }
